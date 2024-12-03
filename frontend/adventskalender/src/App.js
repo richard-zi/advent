1588ac17e550e4311cc20208ede5f266c66932f4
Richard Zimmermann <richard-z@outlook.de>
2024-12-03 08:49:47 +0100

    remove password

--- frontend/adventskalender/src/App.js
+++ frontend/adventskalender/src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdventCalendar from './components/AdventCalendar';
 import Admin from './admin/Admin';
-import PasswordProtection from './components/PasswordProtection';
+// import PasswordProtection from './components/PasswordProtection';
 
 function App() {
   return (
-    <PasswordProtection>
+    // <PasswordProtection>
       <Router>
         <Routes>
           <Route path="/" element={<AdventCalendar />} />
           <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
-    </PasswordProtection>
+    // </PasswordProtection>
   );
 }
 
